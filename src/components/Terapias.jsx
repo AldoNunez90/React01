cbe7e02a9bc8bd13b2f870998286ae651e036bdb
AldoNunez90 <89166838+AldoNunez90@users.noreply.github.com>
2023-07-25 23:17:34 -0300

    segunda prueba final

--- src/components/Terapias.jsx
+++ src/components/Terapias.jsx
@@ -8,9 +8,9 @@ const Terapias = () => {
       <p className="terapiasIntro terapiasTop">
         Todos nuestros tratamientos son de asistencia voluntaria, en caso de ser
         necesario, tras un exhaustivo diagnóstico diferencial, implementando
-        tratamientos.<br/> Contamos con un equipo terapéutico interdisciplinario
+        tratamientos.<br className='brTerapias'/> Contamos con un equipo terapéutico interdisciplinario
         altamente capacitado, conformado por psicólogos, psiquiatras, médicos,
-        docentes, trabajadores sociales,<br/> operadores socio-terapéuticos,
+        docentes, trabajadores sociales,<br className='brTerapias'/> operadores socio-terapéuticos,
         talleristas, entre otros.
       </p>
       <div>
@@ -20,7 +20,7 @@ const Terapias = () => {
       <p className="terapiasIntro" id='contacto'>
         En nuestra comunidad colaboramos con la recuperación de los
         familiares, tanto como la de los pacientes. Tenemos la convicción de que
-        los cambios en la actitud de la familia<br/> favorecerán el progreso de los
+        los cambios en la actitud de la familia<br className='brTerapias'/> favorecerán el progreso de los
         mismos y esto sumando las herramientas aportadas por <span>Clínica Atravezar </span>
         nos conducirán más rápidamente a la recuperación de la enfermedad.
       </p>
